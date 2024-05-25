836681fbe5e65c8a88d7a3d970c22a34fc64b6d3
K0019 <39440047+K0019@users.noreply.github.com>
2024-05-25 19:03:08 +0800

    Obtain AABB of Prefab

--- Assets/EGS/Scripts/LevelGen/LevelGen.cs
+++ Assets/EGS/Scripts/LevelGen/LevelGen.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class LevelGen : MonoBehaviour
 {
+    [SerializeField]
+    private BoundsCalc testBoundsObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(testBoundsObj.bounds);
     }
 
     // Update is called once per frame
